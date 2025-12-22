bbc1db6a827a9f9f4a1200b4b62064a18fc77bc4
Rizul Gupta <rizulgupta2811@gmail.com>
2025-12-23 00:31:39 +0530

    Show cart-item-count in navbar and Added Add-to-cart logic

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,12 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useCart } from "../context/CartContext";
 
 export default function Navbar() {
   const pathname = usePathname();
+  const { cart } = useCart();
+  const cartCount = cart.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
     <nav className="flex items-center justify-between px-6 py-4 border-b border-white">
@@ -31,7 +34,7 @@ export default function Navbar() {
           href="/cart"
           className={pathname === "/cart" ? "underline font-semibold" : "hover:underline"}
         >
-          Cart
+          Cart ({cartCount})
         </Link>
       </div>
     </nav>
