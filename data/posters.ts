5505b7ac6c8d784857c72e2e582be8fa3fe5055a
Rizul Gupta <rizulgupta2811@gmail.com>
2025-12-29 00:16:44 +0530

    Added Razorpay test keys

--- data/posters.ts
+++ data/posters.ts
@@ -1,8 +1,8 @@
 export const posters = [
-  { id: 1, title: "Cyberpunk City", price: 299 },
-  { id: 2, title: "Minimal Mountains", price: 249 },
-  { id: 3, title: "Retro Anime Frame", price: 349 },
-  { id: 4, title: "Abstract Neon Lines", price: 279 },
-  { id: 5, title: "Studio Ghibli Sky", price: 399 },
-  { id: 6, title: "Dark Academia Desk", price: 329 },
+  { id: 1, title: "Cyberpunk City", price: 1 },
+  { id: 2, title: "Minimal Mountains", price: 1 },
+  { id: 3, title: "Retro Anime Frame", price: 1 },
+  { id: 4, title: "Abstract Neon Lines", price: 1 },
+  { id: 5, title: "Studio Ghibli Sky", price: 1 },
+  { id: 6, title: "Dark Academia Desk", price: 1 },
 ];
