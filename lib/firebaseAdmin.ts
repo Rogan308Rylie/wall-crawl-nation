60dce390bb4bb0e3c284af5bafa32611b2da1611
Rizul Gupta <rizulgupta2811@gmail.com>
2025-12-30 23:30:56 +0530

    idk bruh

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,12 +1,18 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const serviceAccountBase64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
+
+  if (!serviceAccountBase64) {
+    throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_BASE64");
+  }
+
+  const serviceAccount = JSON.parse(
+    Buffer.from(serviceAccountBase64, "base64").toString("utf-8")
+  );
+
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
